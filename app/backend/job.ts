8f558cd55fcc0e9696d9f85899aa5a40bd8e6204
LandonHarter <landonharter@outlook.com>
2023-08-19 21:20:29 -0400

    fix build errors

--- app/backend/job.ts
+++ app/backend/job.ts
@@ -2,7 +2,7 @@ import { collection, getDocs, limit, query, setDoc, where, doc } from "firebase/
 import { Job, JobType } from "./types";
 import { firestore } from "./firebase";
 
-export async function getJobs(amount?: number, type?: JobType, salaryRange?: number[]): Job[] {
+export async function getJobs(amount?: number, type?: JobType, salaryRange?: number[]): Promise<Job[]> {
     const jobsRef = collection(firestore, "jobs");
     const jobsQuery = query(jobsRef, limit(amount || 10), where("type", "==", type ? type.toString() : "Full Time"), where("salary", ">=", salaryRange ? salaryRange[0] : 0), where("salary", "<=", salaryRange ? salaryRange[1] : 1000000));
     const jobs = await getDocs(jobsQuery);
